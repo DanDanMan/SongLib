365ac426d1c89e9a89af1099b793199004fde59b
adpa22 <adpa22@gmail.com>
2018-09-27 17:55:02 -0400

    Test Push

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			System.out.println("Test");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
