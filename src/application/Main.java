ced6c63e34c79684b02e8bb3b7fc5a578420bad7
adpa22 <adpa22@gmail.com>
2018-09-28 16:55:37 -0400

    Initial attempt to load FXML file from main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,47 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.SplitPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) throws IOException{
+		//FXMLLoader loader = new FXMLLoader();
+		//loader.setLocation(getClass().getResource("/f2c/view/F2C.fxml"));
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/SongLib/src/view/SongView.fxml"));
+		
+		AnchorPane root = (AnchorPane)loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SongView");
+		primaryStage.setResizable(false);  
+		primaryStage.show();
+		
+		/*try {
+			
+			
+			/*
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			System.out.println("Test");
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 	public static void main(String[] args) {
