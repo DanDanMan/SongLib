2282222b81f4b68bcdf714b276914ae2819eb6b1
adpa22 <adpa22@gmail.com>
2018-09-28 11:10:51 -0400

    Added controller class and fx:id's for buttons in fxml

---
+++ src/view/SongViewController.java
@@ -0,0 +1,10 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SongViewController {
+
+}
