079d6b9c8e59e7c236334d48e827bb04fa344a8e
adpa22 <adpa22@gmail.com>
2018-10-02 23:52:07 -0400

    Deleted uncessary comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,21 +32,7 @@ public class Main extends Application {
 		primaryStage.setResizable(true);  
 		primaryStage.show();
 		
-		/*try {
-			
-			
-			/*
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			System.out.println("Test");
-			
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}*/
+		
 	}
 	
 	public static void main(String[] args) {
